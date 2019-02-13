334f63dcb49b8010b63c721eb945f4cd8a2d2c53
andreyirk <sokolik_85@list.ru>
2019-02-13 09:41:27 +0300

    improve todo card

--- todoapp-react/src/todos/components/TodoBox.js
+++ todoapp-react/src/todos/components/TodoBox.js
@@ -1,29 +1,55 @@
 import React, {Component} from 'react';
 
-import {Button, Card, Grid, Icon, Image, Label, Menu, Table} from 'semantic-ui-react'
+import {Button, Card, Checkbox, Container, Grid, Icon, Image, Label, Menu, Table} from 'semantic-ui-react'
 
 const TodoItems = (props) => (
   <Card.Group stackable itemsPerRow={1}>
     {props.items.map(todo => {
-      return      <Card centered>
-        <Card.Content>
-          <Card.Header>{todo.title}</Card.Header>
-          <Card.Meta>Friends of Elliot</Card.Meta>
-          <Card.Description>
-            {todo.description} <strong>best friends</strong>
-          </Card.Description>
-        </Card.Content>
-        <Card.Content extra>
-          <div className='ui two buttons'>
-            <Button basic color='green'>
-              Approve
-            </Button>
-            <Button basic color='red'>
-              Decline
-            </Button>
-          </div>
-        </Card.Content>
-      </Card>
+      return <Card centered >
+            <Grid padded >
+
+              <Grid.Column width={1}>
+                <Container textAlign='justified' fluid >
+                  <Checkbox  />
+                </Container>
+
+              </Grid.Column>
+              <Grid.Column width={15}>
+
+
+            <Card.Content>
+              <Card.Header>{todo.title}</Card.Header>
+              <Card.Meta>{todo.description}</Card.Meta>
+
+            </Card.Content>
+            <div className="content">
+              <Grid >
+                <Grid.Column width='4' >
+                  <Icon name='circle' color='red' />  Urgently priority
+                </Grid.Column>
+                <Grid.Column  width='4' >
+                  <Card.Meta><Icon name='clock outline' /> 13:00</Card.Meta>
+                </Grid.Column>
+                <Grid.Column  width='4' >
+                  <Card.Meta><Icon name='bell outline' /> 1 hour</Card.Meta>
+                </Grid.Column>
+              </Grid>
+            </div>
+            <Card.Content extra>
+              <div className='ui two buttons'>
+                <Button basic color='green'>
+                  Approve
+                </Button>
+                <Button basic color='red'>
+                  Decline
+                </Button>
+              </div>
+            </Card.Content>
+              </Grid.Column>
+            </Grid>
+          </Card>
+
+
     })}
 
   </Card.Group>
@@ -40,10 +66,10 @@ const TodoSideBar = (props) => (
 const TodoBox = (props) => {
   return (
     <Grid>
-      <Grid.Column width={3}>
+      <Grid.Column width={2}>
         Menu
       </Grid.Column>
-      <Grid.Column width={12}>
+      <Grid.Column width={14}>
         <TodoItems items={props.items}/>
       </Grid.Column>
 
